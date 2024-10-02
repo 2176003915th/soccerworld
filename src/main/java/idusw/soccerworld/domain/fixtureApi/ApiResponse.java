83106fcfeffcc1b8cfa1bd4618a460d9b2b05727
2176003915th <2176003915th@gmail.com>
2024-10-02 14:13:32 +0900

    commit first
    첫번째

---
+++ src/main/java/idusw/soccerworld/domain/fixtureApi/ApiResponse.java
@@ -0,0 +1,23 @@
+//package idusw.soccerworld.domain.fixtureApi;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import java.util.List;
+//
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//@Getter
+//@Setter
+//public class ApiResponse {
+//
+//        private String get;
+//        private fixtureParameters parameters;
+//        private List<fixtureErrors> errors;
+//        private int results;
+//        private fixturePaging paging;
+//        private List<fixtureResponseItem> response;
+//
+//        // Getters and Setters
+//
+//}
